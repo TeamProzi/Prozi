941b73a0c1c4c5647d121fcfbc7f6df7cb0cefe6
Jose Folgar <f.jose@wustl.edu>
2024-04-07 18:01:48 -0500

    hello

---
+++ Maintenance.js
@@ -0,0 +1,25 @@
+import {
+    StyleSheet,
+    Button,
+    View,
+    Image,
+    SafeAreaView,
+    Text,
+    Alert,
+    Linking,
+    Platform
+  } from 'react-native';
+
+export default function Maintenance() {
+    return (
+        <View>
+            <Text>I AM MAINTENANCE</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontC
+    }
+})

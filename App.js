37b6ac71a2cd97009b179b48f88a37bf4f927345
Jose Folgar <f.jose@wustl.edu>
2024-04-07 16:53:21 -0500

    Created top part of home screen

--- App.js
+++ App.js
@@ -3,15 +3,24 @@ import {
   StyleSheet,
   Button,
   View,
+  Image,
   SafeAreaView,
   Text,
   Alert,
-  Linking
+  Linking,
+  Platform
 } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <View style={styles.titleText}>
+          <Text style={styles.welcome}>Welcome!</Text>
+          <Text style={{fontSize: 15}}>RA | Washington University in St. Louis</Text>
+        </View>
+        <Image source={require("./assets/washuLogo.png")} />
+      </View>
       <Text>Yuh what's up Jose</Text>
       <Button
         title="u should press me"
@@ -30,8 +39,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#94a885',
+    backgroundColor: '#FBFBFB',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
   },
+  titleText: {
+    flexDirection: 'column',
+    padding: 0
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 150,
+    alignItems: 'center',
+    paddingTop: 40,
+    backgroundColor: '#FBFBFB',
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.11,
+        shadowRadius: 13.5,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  welcome: {
+    paddingBottom: 5,
+    fontSize: 20,
+  }
 });

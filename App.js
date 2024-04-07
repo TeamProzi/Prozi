23b8a72e679f1dec56958e4474d5b3ec6db03440
Michael Zaslavskiy <michaelzaslavskiy@Michaels-MacBook-Pro.local>
2024-04-07 00:16:41 -0500

    yes finally woo

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Linking
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Yuh what's up Jose</Text>
+      <Button
+        title="u should press me"
+        color ="#ffffff"
+        onPress={() => Linking.openURL('https://www.youtube.com/watch?v=Z2xooz6844k')}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1023fg',
+    backgroundColor: '#94a885',
     alignItems: 'center',
     justifyContent: 'center',
   },
